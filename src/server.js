da4bcc014eed1e58bf6ab55fc48f17bb101e6578
André Filus <filusandre@gmail.com>
2019-07-15 20:19:31 -0300

    add procfile

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
+require("dotenv").config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-mongoose.connect('mongodb+srv://posup:nuvemposup@cluster0-iji9k.mongodb.net/nuvemavancado?retryWrites=true&w=majority', 
+mongoose.connect(
+    process.env.MONGO_URL, 
     {
         useNewUrlParser: true    
     }
 );
+// mongoose.connect('mongodb+srv://posup:nuvemposup@cluster0-iji9k.mongodb.net/nuvemavancado?retryWrites=true&w=majority', 
+//     {
+//         useNewUrlParser: true    
+//     }
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
